61a82bb68648aaa36b8db89c9ad8a6ce48f6d029
Carl Pulley <c.j.pulley@hud.ac.uk>
2010-04-25 15:30:10 +0100

    Replaced use of db with custom code for dumping memory regions.

--- memory_plugins/honeynet/carvestack.py
+++ memory_plugins/honeynet/carvestack.py
@@ -2,7 +2,7 @@ import re
 import commands
 import os.path
 import pydasm
-import fileobjscan
+import fileobjscan, malfind2
 from forensics.win32.vad import traverse_vad, vad_info
 
 KB = 0x400
@@ -40,6 +40,8 @@ def get_stack_frames(ethread):
         while stack_base >= ebp and ebp > stack_limit:
             eip = self.flat_address_space.read_long(trap_frame.vm.vtop(ebp+4))
             ebp = self.flat_address_space.read_long(trap_frame.vm.vtop(ebp))
+            if ebp == 0x0:
+                return
             yield { 'eip':eip, 'ebp':ebp }
     return [ frame for frame in stack_frame_iterator(ethread) ]
 
@@ -67,21 +69,38 @@ def print_vadinfo(vadroot, addr, win32addr):
         print "  Win32 Start Address: 0x%0.8x"%win32addr
 
 def disasm(addr, size):
-    if not self.process_address_space.is_valid_address(addr):
-        print "    Failed to disassemble"
+    if addr < 0:
         return
     buf = self.process_address_space.read(addr, size)
     offset = 0
     while offset < size:
-        i = pydasm.get_instruction(buf[offset:], pydasm.MODE_32)
-        if not i: 
-            print "    0x%0.8x: ??"%(addr+offset)
-            offset += 1 
+        if self.process_address_space.is_valid_address(addr+offset):
+            i = pydasm.get_instruction(buf[offset:], pydasm.MODE_32)
+            if not i: 
+                print "    0x%0.8X: ??"%(addr+offset)
+                offset += 1 
+            else:
+                print "    0x%0.8X: %s"%(addr+offset, pydasm.get_instruction_string(i, pydasm.FORMAT_INTEL, 0))
+                offset += i.length
         else:
-            print "    0x%0.8x: %s"%(addr+offset, pydasm.get_instruction_string(i, pydasm.FORMAT_INTEL, 0))
-            offset += i.length
+            print "    0x%0.8X: unreadable"%(addr+offset)
+            offset += 4
 
-def carvestack(ethread, address_check=None):
+def dump_stack_frame(address, length=0x80, width=16):
+    print "  Frame Dump:"
+    if length % 4 != 0:
+        length = (length+4) - (length%4)
+    print "               00 01 02 03 04 05 06 07 08 09 0A 0B 0C 0D 0E 0F"
+    FILTER=''.join([(len(repr(chr(x)))==3) and chr(x) or '.' for x in range(256)])
+    N=0
+    while N < length:
+        data = ' '.join([ "%02X"%ord(x) if x else '??' for x in [ self.process_address_space.read(address+N+i, 1) for i in range(0, width) ] ])
+        hexa = ''.join([ x.translate(FILTER) if x else '.' for x in [ self.process_address_space.read(address+N+i, 1) for i in range(0, width) ] ])
+        print "    0x%08X %-*s %s"%(address+N, width*3, data, hexa)
+        N += width
+    print
+
+def carvestack(ethread):
     print "Process ID: %d"%ethread.Cid.UniqueProcess.v()
     print "Thread ID: %d"%ethread.Cid.UniqueThread.v()
     if (ethread.CrossThreadFlags >> 0) & 1:
@@ -108,23 +127,19 @@ def carvestack(ethread, address_check=None):
     stack_frames = get_stack_frames(ethread)
     if stack_frames == []:
         print "  Dump:"
-        db(teb.NtTib.StackLimit.v()-0x40, 0x8d)
+        dump_stack_frame(teb.NtTib.StackLimit.v()-0x40, 0x8d)
     else:
         print "Stack Unwind ==>"
         for frame in stack_frames:
-            if address_check != None and address_check(frame['eip']):
-                print "  EIP [**]: 0x%0.8x"%frame['eip']
-            else:
-                print "  EIP: 0x%0.8x"%frame['eip']
+            print "  EIP: 0x%0.8x"%frame['eip']
             print "  EBP: 0x%0.8x"%frame['ebp']
             if frame['ebp'] >= 0x80:
-                db(frame['ebp']-0x40, 0x8d)
-            if frame != stack_frames[-1]:
-                retn_frame = stack_frames[stack_frames.index(frame)+1]
+                dump_stack_frame(frame['ebp']-0x40, 0x8d)
+            if frame['eip'] > 0x20:
                 print "  Calling Code Dissassembly:"
-                disasm(retn_frame['eip']-0x20, 0x21)
+                disasm(frame['eip']-0x20, 0x21)
             print "-"*5
-    # FIXME: w32thread code doesn't appear to work!?
+    # FIXME: w32thread code doesn't appear to work as expected!?
     w32thread = Object('_ETHREAD', ethread.Tcb.Win32Thread.v(), self.kernel_address_space, profile=self.eproc.profile)
     if w32thread.is_valid() and w32thread.offset >= 0x80000000:
         print "  GUI Process: %d"%w32thread.Cid.UniqueProcess.v()
