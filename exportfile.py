f75b487279218cc5cb413a34435fc4f5c0f0075b
Carl Pulley <c.pulley@acme-labs.org.uk>
2011-11-01 15:06:48 +0000

    Initial attempts at generating a DFXML formatted file as output

--- exportfile.py
+++ exportfile.py
@@ -50,61 +50,14 @@ class ExportFile(filescan.FileScan):
 	Given a PID, _EPROCESS or _FILE_OBJECT, extract the associated (or given) 
 	_FILE_OBJECT's from memory.
 	
-	Exported files are written to a user-defined dump directory (--dir). 
-	Contiguous retrievable pages are written to a file named using the 
-	retrieved virtual addresses:
-	
-	  pages from _SHARED_CACHE_MAP are saved in files named cache.0xXX-0xXX.dmp.MD5
-	
-	  pages from _CONTROL_AREA are saved in files named direct.0xXX-0xXX.dmp.MD5
-	
-	where MD5 stands for the hash of the files contents.
-	
-	Pages that can not be retrieved from memory are saved as pages filled in 
-	with a given fill byte (--fill).
-	
-	In addition, a "this" file is created (a sector "copy" of the file on disk) 
-	- this is an aggregated reconstruction based on the retrievable pages above 
-	and, with non-retrievable pages substitued by fill-byte pages (--fill). Some 
-	slack space editing may still be necessary to retrieve the original (user 
-	view) file contents. All exported files are placed into a common directory 
-	(base of which is determined by --dir) whose name and path agree with that 
-	located in _FILE_OBJECT (modulo a unix/linux path naming convention).
+	All exported files are written to a DFXML file. This XML file may then be 
+	processed using various AFFLib compatible tools (e.g. PyFlag).
 	
 	This plugin is particularly useful when one expects memory to be fragmented, 
 	and so, linear carving techniques (e.g. using scalpel or foremost) might be 
 	expected to fail. See reference [5] (below) for more information regarding 
 	some of the forensic benefits of accessing files via the _FILE_OBJECT data 
 	structure.
-	
-	EXAMPLE:
-	
-	_FILE_OBJECT at 0x81CE4868 is a file named:
-	  \\\\Program Files\\\\Mozilla Firefox\\\\chrome\\\\en-US.jar
-	with file size 20992 B and has recoverable pages in memory covering file 
-	offsets:
-	  0x43000 -> 0x45FFF
-	  0x47000 -> 0x47FFF
-	  0x51000 -> 0x52FFF
-	Then:
-	  volatility exportfile -f SAMPLE --fobj 0x82106940 --dir EXAMPLE1 --fill 0xff
-	would produce:
-	  EXAMPLE1/
-		Program Files/
-		  Mozilla Firefox/
-		    chrome/
-		      en-US.jar/
-		        this
-		        cache.0x43000-0x45FFF.dmp.MD5
-		        cache.0x47000-0x47FFF.dmp.MD5
-		        cache.0x51000-0x52FFF.dmp.MD5
-	Where this = fillpages(0xFF)
-	             + cache.0x43000-0x45FFF.dmp.MD5
-	             + fillPages(0xFF)
-	             + cache.0x47000-0x47FFF.dmp.MD5
-	             + fillPages(0xFF)
-	             + cache.0x51000-0x52FFF.dmp.MD5
-	and fillPages(0xFF) is a collection of pages filled with the byte 0xFF.
 	
 	REFERENCES:
 	[1] Russinovich, M., Solomon, D.A. & Ionescu, A., 2009. Windows Internals: 
@@ -138,13 +91,12 @@ class ExportFile(filescan.FileScan):
 
 	def __init__(self, config, *args):
 		filescan.FileScan.__init__(self, config, *args)
-		config.add_option("fill", default = 0, type = 'int', action = 'store', help = "Fill character (byte) for padding out missing pages")
+		# FIXME: do we need a --dir option?
 		config.add_option("dir", short_option = 'D', type = 'str', action = 'store', help = "Directory in which to save exported files")
 		config.add_option("pid", type = 'int', action = 'store', help = "Extract all associated _FILE_OBJECT's from a PID")
 		config.add_option("eproc", type = 'int', action = 'store', help = "Extract all associated _FILE_OBJECT's from an _EPROCESS offset (kernel address)")
 		config.add_option("fobj", type = 'int', action = 'store', help = "Extract a given _FILE_OBJECT offset (physical address)")
 		config.add_option("pool", default = False, action = 'store_true', help = "Extract all _FILE_OBJECT's found by searching the pool")
-		config.add_option("reconstruct", default = False, action = 'store_true', help = "Do not export file objects, perform file reconstruction on previously extracted file pages")
 
 	def calculate(self):
 		self.kernel_address_space = utils.load_as(self._config)
@@ -169,11 +121,7 @@ class ExportFile(filescan.FileScan):
 			# --fobj
 			try:
 				file_object = obj.Object("_FILE_OBJECT", offset = self._config.fobj, vm = self.flat_address_space)
-				if bool(self._config.reconstruct):
-					# --reconstruct
-					return [ (file_object, self.parse_string(file_object.FileName)) ]
-				else:
-					return filter(None, [ self.dump_file_object(file_object) ])
+				return filter(None, [ self.dump_file_object(file_object) ])
 			except ExportException as exn:
 				debug.error(exn)
 		else:
@@ -188,11 +136,7 @@ class ExportFile(filescan.FileScan):
 				if h.get_object_type() == "File":
 					file_obj = obj.Object("_FILE_OBJECT", h.Body.obj_offset, h.obj_vm)
 					try:
-						if bool(self._config.reconstruct):
-							# --reconstruct
-							result += [ (file_obj, self.parse_string(file_obj.FileName)) ]
-						else:
-							result += [ self.dump_file_object(file_obj) ]
+						result += [ self.dump_file_object(file_obj) ]
 					except ExportException as exn:
 						debug.warning(exn)
 		return filter(None, result)
@@ -201,42 +145,32 @@ class ExportFile(filescan.FileScan):
 		result = []
 		for object_obj, file_obj, name in filescan.FileScan.calculate(self):
 			try:
-				if bool(self._config.reconstruct):
-					# --reconstruct
-					result += [ (file_obj, name) ]
-				else:
-					result += [ self.dump_file_object(file_obj) ]
+				result += [ self.dump_file_object(file_obj) ]
 			except ExportException as exn:
 				debug.warning(exn)
 		return filter(None, result)
 
-	def render_text(self, outfd, data):
-		base_dir = os.path.abspath(self._config.dir)
-		if bool(self._config.reconstruct):
-			# Perform no file extraction, simply reconstruct existing extracted file pages
-			for file_object, file_name in data:
-				file_name_path = re.sub(r'//', '/', re.sub(r'[\\:]', '/', base_dir + "/" + file_name))
-				self.reconstruct_file(outfd, file_object, file_name_path)
-		else:
-			# Process extracted file page data and then perform file reconstruction upon the results
-			for file_data in data:
-				for data_type, fobj_inst, file_name, file_size, extracted_file_data in file_data:
-					# FIXME: fix this hacky way of creating directory structures
-					file_name_path = re.sub(r'//', '/', re.sub(r'[\\:]', '/', base_dir + "/" + file_name))
-					if not(os.path.exists(file_name_path)):
-						exe.getoutput("mkdir -p {0}".format(re.escape(file_name_path)))
-					outfd.write("#"*20)
-					outfd.write("\nExporting {0} [_FILE_OBJECT @ 0x{1:08X}]:\n  {2}\n\n".format(data_type, fobj_inst.v(), file_name))
-					if data_type == "_SHARED_CACHE_MAP":
-						# _SHARED_CACHE_MAP processing
-						self.dump_shared_pages(outfd, data, fobj_inst, file_name_path, file_size, extracted_file_data)
-					elif data_type == "_CONTROL_AREA":
-						# _CONTROL_AREA processing
-						self.dump_control_pages(outfd, data, fobj_inst, file_name_path, extracted_file_data)
-					self.reconstruct_file(outfd, fobj_inst, file_name_path)
-
-	def render_sql(self, outfd, data):
-		debug.error("TODO: not implemented yet!")
+	def render_xml(self, outfd, data):
+		# Process extracted file page data and then perform file reconstruction upon the results
+		outfd.write("<dfxml version=\"1.0\">\n")
+		for file_data in data:
+			for data_type, fobj_inst, file_name, file_size, extracted_file_data in file_data:
+				outfd.write("  <fileobject>\n")
+				outfd.write("    <filename>{0}</filename>\n".format(file_name))
+				outfd.write("\nExporting {0} [_FILE_OBJECT @ 0x{1:08X}]:\n  {2}\n\n".format(data_type, fobj_inst.v(), file_name))
+				if data_type == "_SHARED_CACHE_MAP":
+					# _SHARED_CACHE_MAP processing
+					outfd.write("    <filesize>{0}</filesize>\n".format(file_size))
+					outfd.write("    <byte_runs>\n")
+					self.dump_shared_pages(outfd, data, fobj_inst, file_name, file_size, extracted_file_data)
+					outfd.write("    </byte_runs>\n")
+				elif data_type == "_CONTROL_AREA":
+					# _CONTROL_AREA processing
+					outfd.write("    <byte_runs>\n")
+					self.dump_control_pages(outfd, data, fobj_inst, file_name, extracted_file_data)
+					outfd.write("    </byte_runs>\n")
+				outfd.write("  </fileobject>\n")
+		outfd.write("</dfxml>\n")
 
 	KB = 0x400
 	_1KB = KB
@@ -360,84 +294,25 @@ class ExportFile(filescan.FileScan):
 			sector += len(sector_data)*8
 		return result
 
-	def dump_data(self, outfd, data, dump_file, start_addr, end_addr):
-		if not os.path.exists(dump_file):
-			with open(dump_file, 'wb') as fobj:
-				fobj.write(data)
-			outfd.write("Dumped File Offset Range: 0x{0:08X} -> 0x{1:08X}\n".format(start_addr, end_addr))
-		else:
-			outfd.write("..Skipping File Offset Range: 0x{0:08X} -> 0x{1:08X}\n".format(start_addr, end_addr))
+	def dump_data(self, outfd, data, start_addr, end_addr, data_md5, data_sha1):
+		# FIXME: fill in missing format arguments
+		outfd.write("      <byte_run offset=\"{0}\" fs_offset=\"{1}\" imp_offset=\"{2}\" len=\"{3}\">\n".format(, , , end_addr - start_addr + 1))
+		outfd.write("        <hexdigest type=\"md5\">{0}</hexdigest>\n".format(data_md5))
+		outfd.write("        <hexdigest type=\"sha1\">{0}</hexdigest>\n".format(data_sha1))
+		outfd.write("      </byte_run>\n")
 
-	def dump_shared_pages(self, outfd, data, file_object, file_name_path, file_size, extracted_file_data):
+	def dump_shared_pages(self, outfd, data, file_object, file_name, file_size, extracted_file_data):
 		for vacb, start_addr, end_addr in extracted_file_data:
-			vacb_hash = hashlib.md5(vacb).hexdigest()
-			vacb_path = "{0}/cache.0x{1:08X}-0x{2:08X}.dmp.{3}".format(file_name_path, start_addr, end_addr, vacb_hash)
-			self.dump_data(outfd, vacb, vacb_path, start_addr, end_addr)
+			vacb_md5 = hashlib.md5(vacb).hexdigest()
+			vacb_sha1 = hashlib.sha1(vacb).hexdigest()
+			self.dump_data(outfd, vacb, start_addr, end_addr, vacb_md5, vacb_sha1)
 
-	def dump_control_pages(self, outfd, data, file_object, file_name_path, extracted_file_data):
+	def dump_control_pages(self, outfd, data, file_object, file_name, extracted_file_data):
 		sorted_extracted_fobjs = sorted(extracted_file_data, key = lambda tup: tup[1])
 		for page, start_sector in sorted_extracted_fobjs:
 			if page != None:
 				start_addr = start_sector*512
 				end_addr = (start_sector + 8)*512 - 1
-				page_hash = hashlib.md5(page).hexdigest()
-				page_path = "{0}/direct.0x{1:08X}-0x{2:08X}.dmp.{3}".format(file_name_path, start_addr, end_addr, page_hash)
-				self.dump_data(outfd, page, page_path, start_addr, end_addr)
-
-	def reconstruct_file(self, outfd, file_object, file_name_path):
-		def check_for_overlaps(data):
-			# ASSUME: data is sorted by (start_addr, end_addr, md5)
-			while len(data) >= 2:
-				data_type1, start_addr1, end_addr1, data1, hash1 = data[0]
-				data_type2, start_addr2, end_addr2, data2, hash2 = data[1]
-				if start_addr2 <= end_addr1:
-					# Check if the overlap provides a data conflict
-					if start_addr1 == start_addr2 and end_addr1 == end_addr2 and hash1 == hash2:
-						# data overlap provides no conflict
-						pass
-					else:
-						# data conflict exists
-						if os.path.exists("{0}/this".format(file_name_path)):
-							# We're about to fail with reconstruction, eliminate any old "this" files from previous reconstruction attempts
-							os.remove("{0}/this".format(file_name_path))
-						raise ExportException("File Reconstruction Failed: overlapping page conflict (for address range 0x{0:08X}-0x{1:08X}) detected during file reconstruction - please manually fix and use --reconstruct to attempt rebuilding the file".format(start_addr2, end_addr1))
-				else:
-					yield data[0]
-				data = data[1:]
-			if len(data) > 0:
-				yield data[0]
-		
-		outfd.write("[_FILE_OBJECT @ 0x{0:08X}] Reconstructing extracted memory pages from:\n  {1}\n".format(file_object.v(), file_name_path))
-		fill_char = chr(self._config.fill % 256)
-		if os.path.exists(file_name_path):
-			# list files in file_name_path
-			raw_page_dumps = [ f for f in os.listdir(file_name_path) if re.search("(cache|direct)\.0x[a-fA-F0-9]{8}\-0x[a-fA-F0-9]{8}\.dmp(\.[a-fA-F0-9]{32})?$", f) ]
-			# map file list to (dump_type, start_addr, end_addr, data, md5) tuple list
-			page_dumps = []
-			for dump in raw_page_dumps:
-				dump_type, addr_range, ignore, md5 = dump.split(".")
-				start_addr, end_addr = map(lambda x: x[2:], addr_range.split("-"))
-				data = ""
-				with open("{0}/{1}".format(file_name_path, dump), 'r') as fobj:
-					data = fobj.read()
-				if hashlib.md5(data).hexdigest() != md5:
-					debug.error("consistency check failed (MD5 checksum check for {0} failed!)".format(dump))
-				page_dumps += [ (dump_type, int(start_addr, 16), int(end_addr, 16), data, md5) ]
-			try:
-				# check for page overlaps
-				extracted_file_data = check_for_overlaps(sorted(page_dumps, key = lambda x: (x[1], x[2], x[3])))
-				# glue remaining file pages together and save reconstructed file as "this"
-				with open("{0}/this".format(file_name_path), 'wb') as fobj:
-					offset = 0
-					for data_type, start_addr, end_addr, data, data_hash in extracted_file_data:
-						if offset > end_addr:
-							break
-						if offset < start_addr:
-							fobj.write(fill_char*(start_addr - offset))
-						fobj.write(data)
-						offset = end_addr + 1
-				outfd.write("Successfully Reconstructed File\n")
-			except ExportException as exn:
-				debug.warning(exn)
-		else:
-			outfd.write("..Skipping file reconstruction due to a lack of extracted pages\n")
+				page_md5 = hashlib.md5(page).hexdigest()
+				page_sha1 = hashlib.sha1(page).hexdigest()
+				self.dump_data(outfd, page, start_addr, end_addr, page_md5, page_sha1)
